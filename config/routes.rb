077f7c1f562e51802886dd3b98203e8263a402a0
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-30 22:56:43 +0900

    8/30
    Javaトップページの作成（ヘッダー）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/",to:"java#index"
+  root "program#index" #サイトのプログラミング言語選択ページ
+  get "/Java",to:"java#index" #Javaのトップページ
 end
