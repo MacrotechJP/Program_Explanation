5047ef3c0eacb6f23b3ee315192c81524d4a540a
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-08-31 11:02:37 +0900

    rubyページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "program#index" #サイトのプログラミング言語選択ページ
   get "/Java",to:"java#index" #Javaのトップページ
+  get "/Ruby",to:"ruby#index" #Rubyのトップページ
 end
