701e5632f38b580a506e47a30c24b62284263721
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-09-05 20:13:00 +0900

    9/5
    スムーズスクロール 実装

--- app/assets/javascripts/program_index.js
+++ app/assets/javascripts/program_index.js
@@ -1,3 +1,17 @@
+//スムーズスクロールの処理
+$(function(){
+  $('a[href^="#"]').click(function(){
+      var speed = 1000;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+  });
+});
+  
+
+//トップページの変化テキスト処理
 $(function(){
   $(".menu-open-button").click();
   class TextScramble {
@@ -82,17 +96,3 @@ $(function(){
   next()
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
