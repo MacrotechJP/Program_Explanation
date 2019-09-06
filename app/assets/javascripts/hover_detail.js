2f44586e30530f7cbc6d02599ef9d9655ca154ba
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-09-07 00:48:21 +0900

    9/6
    deviseページ作成

---
+++ app/assets/javascripts/hover_detail.js
@@ -0,0 +1,9 @@
+// $(function(){
+//   $(".detail_before span")
+//   .mouseover(function() {
+//     $(this).parent().parent().find(".detail_hover").fadeIn(2000)
+//   })
+//   .mouseout(function() {
+//     // $(this).parent().parent().find(".detail_hover").fadeOut(4000)
+//   });
+// })
