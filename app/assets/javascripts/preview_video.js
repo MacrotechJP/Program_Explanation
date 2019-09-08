28236ceaadeb463467ff9c2af2382949768e73b1
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-09-09 00:41:49 +0900

    9/9
    ビデオ再生位置初期化に伴う、ヘッダーずれ修正

--- app/assets/javascripts/preview_video.js
+++ app/assets/javascripts/preview_video.js
@@ -1,12 +1,12 @@
 $(function(){
   $("#pre").on("click",function(){
+    var video = $('video').get(0);
+    video.currentTime = 0
     $(this).parent().find("video").show();
   })
-  $(document).on('click',   function(e) {
+  $(document).on('click',function(e) {
     if (!$(e.target).closest('#pre').length) {
       $("video").hide();
-      var video = $('video').get(0);
-      video.currentTime = 0
     }
   });
 })
